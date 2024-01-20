c6efa440e3c4e7d40925c4a7db79f27f3d107bbb
蓝天 <eyjian@qq.com>
2024-01-20 11:46:54 +0800

    Add cache

--- model/t_mooon_mailbox_model.go
+++ model/t_mooon_mailbox_model.go
@@ -1,6 +1,9 @@
 package model
 
-import "github.com/zeromicro/go-zero/core/stores/sqlx"
+import (
+	"github.com/zeromicro/go-zero/core/stores/cache"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
 
 var _ TMooonMailboxModel = (*customTMooonMailboxModel)(nil)
 
@@ -9,7 +12,6 @@ type (
 	// and implement the added methods in customTMooonMailboxModel.
 	TMooonMailboxModel interface {
 		tMooonMailboxModel
-		withSession(session sqlx.Session) TMooonMailboxModel
 	}
 
 	customTMooonMailboxModel struct {
@@ -18,12 +20,8 @@ type (
 )
 
 // NewTMooonMailboxModel returns a model for the database table.
-func NewTMooonMailboxModel(conn sqlx.SqlConn) TMooonMailboxModel {
+func NewTMooonMailboxModel(conn sqlx.SqlConn, c cache.CacheConf, opts ...cache.Option) TMooonMailboxModel {
 	return &customTMooonMailboxModel{
-		defaultTMooonMailboxModel: newTMooonMailboxModel(conn),
+		defaultTMooonMailboxModel: newTMooonMailboxModel(conn, c, opts...),
 	}
-}
-
-func (m *customTMooonMailboxModel) withSession(session sqlx.Session) TMooonMailboxModel {
-	return NewTMooonMailboxModel(sqlx.NewSqlConnFromSession(session))
 }
